1b9852fce7aa0391412b90ed1d4741173c2c11c2
Diego Santana <diegovjb916@gmail.com>
2026-06-22 19:22:42 -0300

    script

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 <script>alert("hello word")</script>
 'use strict';
-const swither = document.querySelector(".bnt");
-swither.addEventListener('click', function(){
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function () {
     document.body.classList.toggle('dark-theme')
-});
+
+
+    var className = document.body.className;
+    if (className == 'light-theme') {
+        this.textContent = "dark";
+    }
+    else {
+        this.textContent = "light";
+        console.log('current class name:' + className);
+    }
+
+})
