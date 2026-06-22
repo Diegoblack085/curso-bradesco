79fc6440d21fb95b048ba9f61f05de985df59753
Diego Santana <diegovjb916@gmail.com>
2026-06-22 13:00:22 -0300

    script

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 <script>alert("hello word")</script>
+'use strict';
+const swither = document.querySelector(".bnt");
+swither.addEventListener('click', function(){
+    document.body.classList.toggle('dark-theme')
+});
